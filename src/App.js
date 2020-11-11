c612e0c88c0cd4998c5a92a32c49a61314a4690a
FOUIX <fouix@pc-155.home>
2020-11-11 14:57:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./App.css";
 
+//import du package react-rounter-dom
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+//import composantts Header
+import Header from "./components/Header";
+
+//import de home
+import Home from "./containers/Home";
+
 function App() {
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
